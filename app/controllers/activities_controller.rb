81865f32af576f033c411ed709045dbc2332032b
Marian Clements <marian.clements@fluxfederation.com>
2019-12-05 14:59:09 +1300

    Introduce Role based permissions

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,19 +1,17 @@
 class ActivitiesController < ApplicationController
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
-
-  # GET /activities
-  # GET /activities.json
+  before_action :is_admin?, except: [:index]
+  
   def index
     @activities = Activity.all
   end
 
-  # GET /activities/1
-  # GET /activities/1.json
   def show
   end
 
   # GET /activities/new
   def new
+    
     @activity = Activity.new
   end
 
@@ -21,18 +19,14 @@ class ActivitiesController < ApplicationController
   def edit
   end
 
-  # POST /activities
-  # POST /activities.json
   def create
     @activity = Activity.new(activity_params)
 
     respond_to do |format|
       if @activity.save
         format.html { redirect_to @activity, notice: 'Activity was successfully created.' }
-        format.json { render :show, status: :created, location: @activity }
       else
         format.html { render :new }
-        format.json { render json: @activity.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,10 +37,8 @@ class ActivitiesController < ApplicationController
     respond_to do |format|
       if @activity.update(activity_params)
         format.html { redirect_to @activity, notice: 'Activity was successfully updated.' }
-        format.json { render :show, status: :ok, location: @activity }
       else
         format.html { render :edit }
-        format.json { render json: @activity.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -57,7 +49,6 @@ class ActivitiesController < ApplicationController
     @activity.destroy
     respond_to do |format|
       format.html { redirect_to activities_url, notice: 'Activity was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -70,5 +61,11 @@ class ActivitiesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def activity_params
       params.require(:activity).permit(:name, :description, :score_value)
+    end
+
+    def is_admin?
+      if current_user.user?
+        return redirect_to activities_path, notice: 'You are not authorised to perform that task'
+      end
     end
 end
